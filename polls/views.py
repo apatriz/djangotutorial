0ed3c7fdebf995e0b248d06c958d67c8b6e88093
apatriz <patrizio@msu.edu>
2015-11-28 12:08:15 -0500

    Finished tutorial 4

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return render(request,'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
-	return render(request, "polls/detail.html", {'question':question})
 
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
-	return HttpResponse(response % question_id)
+
+class ResultsView(generic.DeleteView):
+	model= Question
+	template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
@@ -35,7 +38,7 @@ def vote(request, question_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 		# Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
